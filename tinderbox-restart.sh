dab576c6890642691ca6c28df93920b5560a55d0
Diego Elio 'Flameeyes' Pettenò <flameeyes@gmail.com>
2009-12-06 20:06:22 +0100

    Integrate Haskell (ghc) update support.
    Since an update of ghc requires running a special command, we need to
    consider that as a special package.

--- tinderbox-restart.sh
+++ tinderbox-restart.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+reset_emergelog() {
+    sed -i -e '$r /var/log/emerge.log' /var/log/emerge-total.log
+    rm -f /var/log/emerge.log
+}
+
 set -e
 
 echo "#syncing anew upon request" | bti
@@ -10,15 +15,21 @@ echo > /etc/portage/package.mask/currentrun
 
 ./unavailable_installed.py | xargs -r emerge -C
 
-sed -i -e '$r /var/log/emerge.log' /var/log/emerge-total.log
-rm -f /var/log/emerge.log
+reset_emergelog
 
 emerge -1 --selective gcc
 if fgrep -q '>>> emerge' /var/log/emerge.log; then
     ./update-gcc-asneeded.sh
 fi
 
+reset_emergelog
+
+emerge -1 --selective ghc haskell-updater
+if fgrep -q '>>> emerge' /var/log/emerge.log; then
+    echo "running #haskell-updater"
+    /usr/sbin/haskell-updater --upgrade
+fi
+
 emerge -1 --selective glibc portage bti screen avahi nfs-utils gentoolkit java-dep-check portage-utils
 
-sed -i -e '$r /var/log/emerge.log' /var/log/emerge-total.log
-rm -f /var/log/emerge.log
+reset_emergelog
