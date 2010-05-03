0f2984ead4164a7647e71de9bcf381f77980586c
Diego Elio 'Flameeyes' Pettenò <flameeyes@gmail.com>
2010-05-03 20:20:41 +0200

    Try to recover if test dependencies cause cycles that Portage cannot unbreak.

--- emerge-wrapper.sh
+++ emerge-wrapper.sh
@@ -14,7 +14,19 @@ echo -5 | etc-update
 
 if [[ $res != 0 ]]; then
     if ! fgrep -q ">>> emerge" /var/log/emerge.log; then
-	echo "$1 merge #rejected" | bti
+	# Here it means that the merge was rejected; the common case
+	# it's a cyclic dependency that Portage cannot break, which is
+	# unfortunately common when enabling tests e.g. with Ruby-NG
+	# ebuilds. To try recovering from this, try a merge without
+	# test features enabled.
+	if [[ -z "${FEATURES}" ]]; then
+	    FEATURES=-test $0 "$@"
+	else
+	    # This only hits the second time, so we're safely assuming
+	    # that the package will reject to be merged as it is, why,
+	    # we'll have to check.
+	    echo "$1 merge #rejected" | bti
+	fi
     fi
 fi
 
