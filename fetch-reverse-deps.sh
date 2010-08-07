ca9b8af6af8c7ef32573271c99746c080d4e3ecb
Diego Elio 'Flameeyes' Pettenò <flameeyes@gmail.com>
2010-08-07 12:53:10 +0200

    Allow fetching rdeps for multiple packages, folded together, and filter blockers.

--- fetch-reverse-deps.sh
+++ fetch-reverse-deps.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-wget -q -O- http://tinderbox.dev.gentoo.org/misc/{r,d}index/$1 | \
+(
+    for atom in $@; do
+        wget -q -O- http://tinderbox.dev.gentoo.org/misc/{r,d}index/${atom}
+    done
+) | egrep -v '^\[B\]' | \
     xargs -n1 qatom | \
     cut -d ' ' -f 1-2 | \
     tr ' ' '/' | \
