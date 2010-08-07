6a3da1a16d5b58c37e02f4784030b055efa588e1
Diego Elio 'Flameeyes' Pettenò <flameeyes@gmail.com>
2010-08-07 12:58:04 +0200

    Simplify reverse-deps fetching.
    Since qatom+cut+tr is not extremely fast, sort and remove duplicate lines
    before, then only uniq them to drop the atoms to single entries.

--- fetch-reverse-deps.sh
+++ fetch-reverse-deps.sh
@@ -4,8 +4,8 @@
     for atom in $@; do
         wget -q -O- http://tinderbox.dev.gentoo.org/misc/{r,d}index/${atom}
     done
-) | egrep -v '^\[B\]' | \
+) | egrep -v '^\[B\]' | sort -u | \
     xargs -n1 qatom | \
     cut -d ' ' -f 1-2 | \
     tr ' ' '/' | \
-    sort -u
+    uniq
