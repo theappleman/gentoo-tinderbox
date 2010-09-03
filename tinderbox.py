40c943a385fe8640b97bd3de9e1c43259ecbef74
Diego Elio 'Flameeyes' Pettenò <flameeyes@gmail.com>
2010-09-03 02:32:47 +0200

    Add a proper license to all the scripts running the Tinderbox.
    Since most of them are really simple scripts that don't really warrant for
    strong copyleft, they are all licensed under ISC license.
    
    Copyright is, depending on the file, mine, Zac's, or both.

--- tinderbox.py
+++ tinderbox.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python
+#
+# Copyright © 2008-2010 Zac Medico <zmedico@gentoo.org>
+# Copyright © 2008-2010 Diego Elio Pettenò <flameeyes@gentoo.org>
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND INTERNET SOFTWARE CONSORTIUM DISCLAIMS
+# ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL INTERNET SOFTWARE
+# CONSORTIUM BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+# DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
+# PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS
+# ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
+# SOFTWARE.
 
 seconds_per_week = 7 * 24 * 60 * 60
 # reinstall_period = None
--- update-gcc-asneeded.sh
+++ update-gcc-asneeded.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
+#
+# Copyright © 2007-2010 Diego Elio Pettenò <flameeyes@gentoo.org>
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND INTERNET SOFTWARE CONSORTIUM DISCLAIMS
+# ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL INTERNET SOFTWARE
+# CONSORTIUM BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+# DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
+# PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS
+# ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
+# SOFTWARE.
 
 export SPECSFILE=$(dirname "$(gcc -print-libgcc-file-name)")/asneeded.specs
 export CURRPROFILE=/etc/env.d/gcc/$(gcc-config -c)
