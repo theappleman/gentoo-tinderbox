c022658e025a9b11b920289913ab42cfedd25f22
Diego Elio Pettenò <flameeyes@gmail.com>
2010-09-08 12:58:21 +0200

    Don't check whether the file is executable but rather if it exists.

--- tinderbox-continuous.sh
+++ tinderbox-continuous.sh
@@ -17,7 +17,7 @@
 
 tboxdir=$(dirname $0)
 
-until [ -e /var/run/tinderbox.pleasestop ]; do
+until [ -f /var/run/tinderbox.pleasestop ]; do
     ${tboxdir}/tinderbox-restart.sh
 
     head -n 200 /var/cache/tinderbox/queue | xargs -n1 ${tboxdir}/emerge-wrapper.sh
